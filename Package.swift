f1bb823cc2a8b964f1380887263f1185ecfbc45a
levitatingpineapple <auth@levitatingpineapple.com>
2020-12-25 14:25:42 +0100

    Package platform

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
 	name: "DocumentsFolder",
+	platforms: [.iOS(.v13)], 
 	products: [.library(name: "DocumentsFolder", targets: ["DocumentsFolder"])],
 	targets: [.target(name: "DocumentsFolder")]
 )
